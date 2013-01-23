33ad08ac6a69cbb9e70cbcee1e88196e6507fb9e
nbarone <xnickbarone@gmail.com>
2013-01-22 19:31:10 -0700

    fixed #17 removed getPageNavMobile

--- controllers/brdi/Portal/Component/Nav.php
+++ controllers/brdi/Portal/Component/Nav.php
@@ -44,7 +44,7 @@ class brdi_Portal_Component_Nav extends brdi_Portal_Component
 
 		$template = $this->parseToken($template, "token://clientName", $this->getClientName());
 		$template = $this->parseToken($template, "token://pageNav", $this->getPageNav());
-		$template = $this->parseToken($template, "token://mobileNav", $this->getPageNavMobile());
+		$template = $this->parseToken($template, "token://mobileNav", $this->getPageNav());
 
 		$template = $this->buildComponentWrapper($template, $config);
 
@@ -54,7 +54,7 @@ class brdi_Portal_Component_Nav extends brdi_Portal_Component
 	/**
 	 * getPageNav
 	 *
-	 * Returns html formatted nav for Page/Tablet display
+	 * Returns html formatted nav
 	 *
 	 * @return String Html formatted nav
 	 */
@@ -83,44 +83,6 @@ class brdi_Portal_Component_Nav extends brdi_Portal_Component
 				if($navitem === end($this->nav)) $nav_builder .= "<li class='divider-vertical'></li>";
 				$nav_raw .= $nav_builder;
 				unset($li_class, $page_config);
-			}
-		}
-		// build nav for phone
-		return $nav_raw;
-	}
-
-	/**
-	 * getPageNavMobile
-	 *
-	 * Returns html formatted nav for Mobile display
-	 *
-	 * @return String Html formatted nav
-	 */
-	private function getPageNavMobile()
-	{
-		$nav_raw = "";
-		foreach($this->nav as $navitem)
-		{
-			include($this->getConfigOverride("/page/".$navitem.".php"));
-			if($page_config)
-			{
-				if($navitem == $this->nav[0])
-				{
-					$nav_builder = "<li class='divider first'></li>";
-				}
-				else
-				{
-					$nav_builder = "<li class='divider'></li>";
-				}
-				$li_class = "";
-				if($this->isThisPage($navitem)) $li_class = " class='active'";
-				$nav_builder .= "<li{$li_class}>";
-				$nav_builder .= "<a href='{$page_config['href']}'";
-				if(isset($page_config['class'])) $nav_builder .= " class='{$page_config['class']}'";
-				$nav_builder .= ">{$page_config['title']}</a></li>";
-				if($navitem === end($this->nav)) $nav_builder .= "<li class='divider-vertical'></li>";
-				$nav_raw .= $nav_builder;
-				unset($li_class);
 			}
 		}
 		// build nav for phone
