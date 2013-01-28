f1aecb0f43b3ab89aad91b3f84c118b782c65cc0
nbarone <xnickbarone@gmail.com>
2013-01-27 17:49:07 -0700

    fixed #21 Added buttons to location component

--- config/default/assets/templates/components/location/view.php
+++ config/default/assets/templates/components/location/view.php
@@ -3,3 +3,10 @@
 <div class='Component_Location_Street'>!{token://location_street}</div>
 <div class='Component_Location_CityStateZip'>!{token://location_city}, !{token://location_state} !{token://location_zip}</div>
 <div class='Component_Location_Phone'>!{token://location_phone}</div>
+
+<div class='Component_Location_Button_Call show_phone intent_button'>
+	<a href="tel://!{token://location_phone}" target="_blank"><button class="btn btn-info btn-block btn-large">Call Us</button></a>
+</div>
+<div class='Component_Location_Button_Directions show_phone intent_button'>
+	<a href="http://maps.google.com/?q=!{token://location_url_encoded}" target="_blank"><button class="btn btn-info btn-block btn-large">Get Directions</button></a>
+</div>
--- controllers/brdi/Portal/Component/Location.php
+++ controllers/brdi/Portal/Component/Location.php
@@ -10,6 +10,8 @@ class brdi_Portal_Component_Location extends brdi_Portal_Component
 	public $location;
 	
 	private $_brdi_Portal_Component_Location = array(
+		'show_call_button' => true,
+		'show_maps_button' => true,
 		'assets' => array(
 			'stylesheets' => array(
 				'assets/stylesheets/components/location/location.css',
@@ -54,6 +56,9 @@ class brdi_Portal_Component_Location extends brdi_Portal_Component
 		$template = $this->parseToken($template, "token://location_city", $this->getLocationCity());
 		$template = $this->parseToken($template, "token://location_state", $this->getLocationState());
 		$template = $this->parseToken($template, "token://location_zip", $this->getLocationZip());
+		
+		$url_encoded_location = urlencode($this->getLocationStreet()." ".$this->getLocationCity()." ".$this->getLocationState()." ".$this->getLocationZip());
+		$template = $this->parseToken($template, "token://location_url_encoded", $url_encoded_location);
 
 		$template = $this->buildComponentWrapper($template, $config);
 
