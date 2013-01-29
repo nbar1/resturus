a0334ca304bd42fcc2bad20ef1c0636436c36f63
nbarone <xnickbarone@gmail.com>
2013-01-28 17:14:08 -0700

    fixed #19 Created css compiler, added additional browser caching, gitignores .htaccess updated

--- brdi/scripts/css.php
+++ brdi/scripts/css.php
@@ -1,24 +1,25 @@
 <?php
-header("Content-Style-Type: text/css");
+ob_start ("ob_gzhandler");
+header("content-type: text/css; charset: UTF-8");
+header("Cache-Control: max-age=1800");
+
+/* your css files */
 $css = json_decode(urldecode($_GET['load']));
 
 // include globals
-array_unshift($css, "config/default/assets/stylesheets/global/global.css");
-array_unshift($css, "http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css");
-array_unshift($css, "http://fonts.googleapis.com/css?family=Prata");
+if(!isset($_GET['exclude_global'])) array_unshift($css, "config/default/assets/stylesheets/global/global.css");
+if(!isset($_GET['exclude_bootstrap'])) array_unshift($css, "http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css");
+if(!isset($_GET['exclude_font'])) array_unshift($css, "http://fonts.googleapis.com/css?family=Prata");
 
 foreach($css as $css_file)
 {
 	$old_css = $css_file;
 	if(strpos($css_file, "http") !== 0)
 	{
-		$css_file = "/home1/thegogre/www/resturus/" . $css_file;
+		$css_file = "../../" . $css_file;
 	}
-	$css_output .=  "\n\n\n/** {$old_css} **/\n\n";
-	$css_output .= file_get_contents($css_file);
+	echo "\n\n/** file: {$old_css} **/\n";
+
+	echo str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', file_get_contents($css_file));
 }
-
-$css_output = str_replace("}", "} ", $css_output);
-echo $css_output;
-
 ?>
